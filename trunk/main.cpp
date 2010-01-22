7c84b8532944c66922ebf2448ac46c724d0b32ef
silveira.kaue@gmail.com <silveira.kaue@gmail.com@f233e958-e0f9-11de-8065-53a27c5290e0>
2010-01-22 18:46:12 +0000

    Commiting all robopet. Communication done. All the arrows working. Did some corrections of the makefile of the robopet-communication and created the makefile of the gui.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -4,49 +4,35 @@
 #include "ssl_server.h"
 
 RoboCupSSLServer trackertoai(PORT_TRACKER_TO_AI, IP_TRACKER_TO_AI);
-RoboCupSSLClient simtotracker(PORT_SIM_TO_TRACKER, IP_SIM_TO_TRACKER);
+RoboCupSSLClient simtotracker(PORT_SIM_TO_TRACKER, IP_SIM_TO_TRACKER),
+				 aitotracker(PORT_AI_TO_TRACKER, IP_AI_TO_TRACKER);
 
 int DEBUG = 1;
 
+void receiveFromSim();
+void receiveFromAI();
+
 void receive()
 {
+	receiveFromSim();
+	receiveFromAI();
+}
+
+void receiveFromSim()
+{
 	SSL_WrapperPacket packet;
-	if (simtotracker.receive(packet)) {
-		printf("----------------------------\n");
-		printf("TestClient Received ");
+	if (simtotracker.receive(packet) && packet.has_simtotracker()) {
+		printf("----------------------------");
+		printf("Received SIM-To-TRACKER!\n");
+	}
+}
 
-		if (packet.has_simtotracker())
-		{
-			printf("SIM-To-TRACKER!\n");
-		}
-
-/*
-		if (packet.has_trackertoai())
-		{
-			if(DEBUG)
-			{
-				printf("Tracker-To-AI!\n");
-				printf("Ball: <%d, %d>\n", packet.trackertoai().ball().x(),
-				 							packet.trackertoai().ball().y());
-			
-			
-				for(int i=0; i<packet.trackertoai().robots_blue_size(); i++)
-					printf("Blue Robot[%d]: <%d, %d>\n", i,
-											packet.trackertoai().robots_blue(i).x(),
-											packet.trackertoai().robots_blue(i).y());
-				for(int i=0; i<packet.trackertoai().robots_yellow_size(); i++)
-					printf("Yellow Robot[%d]: <%d, %d>\n", i,
-											packet.trackertoai().robots_yellow(i).x(),
-											packet.trackertoai().robots_yellow(i).y());
-			}
-			
-			_ball.setCurrentPosition(Point(packet.trackertoai().ball().x(),
-				 						   packet.trackertoai().ball().y()));
-				 						   
-			for(int i=0; i<packet.trackertoai().robots_blue_size(); i++)
-				_players[i].setCurrentPosition(Point(packet.trackertoai().robots_blue(i).x(),
-												     packet.trackertoai().robots_blue(i).y()));
-		}//*/
+void receiveFromAI()
+{
+	SSL_WrapperPacket packet;
+	if (aitotracker.receive(packet) && packet.has_aitotracker()) {
+		printf("----------------------------");
+		printf("Received AI-To-TRACKER!\n");
 	}
 }
 
@@ -56,23 +42,25 @@ void send()
 
 	 TrackerToAI *trackertoaiPacket = packet.mutable_trackertoai();
 	 BallTracker *b = trackertoaiPacket->mutable_ball();
-	 
+
 	 b->set_x(77);
 	 b->set_y(666);
-	 
+
 	 trackertoai.send(packet);
+	printf("Sent Tracker-To-AI\n");
 }
 
 int main()
 {
 	printf("Tracker Running!\n");
-	
+
 	simtotracker.open(false);
-	
+	aitotracker.open(false);
+
 	printf("Press <Enter> to open connection with client...\n");
 	getchar();
 	trackertoai.open();
-	
+
 	while(1) {
 		receive();
 		send();
